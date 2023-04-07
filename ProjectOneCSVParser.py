0119d65d1ca7157035006e7620b5f9bfaa88fdd9
DanielEJones <126533025+DanielEJones@users.noreply.github.com>
2023-04-07 15:04:18 +0800

    Splits file into rows and stores rows in list 'data'. Now reads file one char at a time so as not to overload memory. Will not include double quotes in data

--- ProjectOneCSVParser.py
+++ ProjectOneCSVParser.py
@@ -1,14 +1,41 @@
 with open('countries.csv', 'r') as file:
 
+    # Initialise variables
     string = ''
-    out = []
+    data = []
+    line = []
+    index = 0
 
-    for char in file.read():
-        # if the current char is a comma or a newline, add the current string to the output list and continue
-        if char in ',\n':
-            out.append(string)
-            # clear string in preparation for next element
+    # read the first character in the file.
+    char = file.read(1)
+
+    # while there are unread characters in the file:
+    while char:
+
+        # get current char
+        file.seek(index)
+        char = file.read(1)
+
+        # update index now; statements break loop so can't increment at end
+        index += 1
+
+        # skip double quotes
+        if '\"' in char:
+            continue
+
+        # a comma marks a new item in row, so append current string and move on to next char
+        if char in ',':
+            line.append(string)
             string = ''
             continue
-        # add current char to the element string
+
+        # \n marks end of a row, so append row to data and reset
+        if char in '\n':
+            line.append(string)
+            data.append(line)
+            line = []
+            string = ''
+            continue
+
+        # if we have made it this far, the char is plaintext and so append it to the current string
         string += char
