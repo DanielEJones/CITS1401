3b1ac790c423b6ec1af4d57f2213c362df96783c
DanielEJones <126533025+DanielEJones@users.noreply.github.com>
2023-05-12 20:28:01 +0800

    New commit

--- Project 2/CSVParser.py
+++ Project 2/CSVParser.py
@@ -1,50 +1,121 @@
-def csv_reader(file, delimiter=',', escape='\"', linebreak='\n') -> list:
-    escaped, string, row = False, '', []
-    for character in file:
-        if escape in character:
-            escaped = not escaped
-            continue
-        if character in {delimiter, linebreak} and not escaped:
-            row.append(string)
-            string = ''
-            if character == linebreak:
-                yield row
-                row = []
-            continue
-        string += character
-    if string:
-        row.append(string)
-        yield row
+class CSVReader:
 
+    def __init__(self, file, headers=False, escape='\"', delimiter=',', linebreak='\n'):
+        self._file = (character for character in file)
+        self._escape, self._delimiter, self._linebreak = escape, delimiter, linebreak
+        self.headers = self.__next__() if headers else None
 
-def get_header_indexes(received: list, expected=('country', 'population', 'net change', 'land area', 'regions')):
-    received = tuple(heading.lower() for heading in received)
-    return tuple(received.index(header) for header in expected)
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> list[str]:
+        escaped, string, row = False, [], []
+        for character in self._file:
+            if self._escape in character:
+                escaped = not escaped
+                continue
+            if self._delimiter in character and not escaped:
+                row.append(''.join(item for item in string))
+                string = []
+                continue
+            if self._linebreak in character and not escaped:
+                row.append(''.join(item for item in string))
+                return row
+            string.append(character)
+        if string:
+            row.append(''.join(item for item in string))
+            return row
+        raise StopIteration
 
 
 class Country:
 
-    def __init__(self, data, headers):
-        data = [set_type(item) for item in data]
+    NAME: str
+    POPULATION: int
+    NET_CHANGE: int
+    LAND_AREA: int
+    REGION: str
+
+    def __init__(self, data: list[str], headers: tuple[int, ...]):
         self.NAME, self.POPULATION, self.NET_CHANGE, self.LAND_AREA, self.REGION = (data[i] for i in headers)
-        expected_types = (str, int, int, int, str)
-        properties = (self.NAME, self.POPULATION, self.NET_CHANGE, self.LAND_AREA, self.REGION)
-        for type_, property_ in zip(expected_types, properties):
-            if not isinstance(property_, type_) or not property_:
-                raise ValueError(f'{self.NAME!r}: was expecting {type_.__name__!r} but got {type(property_).__name__!r}')
+        self._data = (self.NAME, self.POPULATION, self.NET_CHANGE, self.LAND_AREA, self.REGION)
+
+    def __repr__(self):
+        return f'{self.NAME}=({self.POPULATION=:.3f}, {self.LAND_AREA=}, {self.REGION=})'
+
+    def rank(self):
+        return -self.POPULATION, -self.POPULATION / self.LAND_AREA, self.NAME
+
+    @property
+    def data(self):
+        return self._data
 
 
-def set_type(item: str):
+class CSVCountryHandler:
+
+    _countries: list[Country]
+    _header_indexes: tuple[int, ...]
+
+    def __init__(self, expected_headers: tuple[str, ...]):
+        self._countries = []
+        self._headers = expected_headers
+
+    @staticmethod
+    def _type_data(item: str) -> [int, str]:
+        return int(item) if item.lstrip('-+').isdigit() else item
+
+    @staticmethod
+    def _country_is_valid(country: Country, expected_types: tuple) -> bool:
+        return tuple(type(item) for item in country.data) == expected_types
+
+    def on_header(self, received: list[str]) -> None:
+        received = tuple(heading.lower() for heading in received)
+        try:
+            self._header_indexes = tuple(received.index(header) for header in self._headers)
+        except ValueError:
+            raise ValueError('At least one header missing.')
+
+    def on_row(self, data: list[str]) -> None:
+
+        if not hasattr(self, '_header_indexes'):
+            raise AttributeError('No Headers')
+
+        if any(item in {None, '\0', ''} for item in data):
+            raise ValueError('Row contains missing data')
+
+        data = [self._type_data(item) for item in data]
+        c = Country(data, self._header_indexes)
+
+        if not self._country_is_valid(c, (str, int, int, int, str)):
+            raise ValueError('Type Mismatch')
+
+        self._countries.append(c)
+
+    def on_finish(self) -> [dict, dict]:
+        print('\n'.join(f'{country}' for country in sorted(self._countries, key=Country.rank)))
+        return {}, {}
+
+
+# Covered by on_header() method in "CSVCountryHandler
+def get_header_indexes(received: list[str], expected: tuple[str, ...]) -> tuple[int, ...]:
+    received = tuple(heading.lower() for heading in received)
+    return tuple(received.index(header) for header in expected)
+
+
+# Covered by _type_data() method in "CSVCountryHandler"
+def set_type(item: str) -> [str, int]:
     return int(item) if item.lstrip('-+').isdigit() else item
 
 
 def main(file):
-    with open(file, 'r') as f:
-        received_headers, *data = csv_reader(f.read())
-        header_indexes = get_header_indexes(received_headers)
-        countries = []
-        for country in data:
-            pass
+
+    with open(file, 'r') as csv_file:
+        reader = CSVReader(csv_file.read(), headers=True)
+        handler = CSVCountryHandler(expected_headers=('country', 'population', 'net change', 'land area', 'regions'))
+        handler.on_header(reader.headers)
+        for country in reader:
+            handler.on_row(country)
+        _, __ = handler.on_finish()
 
     stats = {'northern america': [80089583.56, 0.7841]}
     northern_america_data = {
--- Project 2/CSVParser_test.py
+++ Project 2/CSVParser_test.py
@@ -14,55 +14,55 @@ class CSVReaderTest(TestCase):
         # Test that values in row are correctly splits at commas.
         test_data = StringIO('this,is,a,csv,file\n')
         expected_data = (['this', 'is', 'a', 'csv', 'file'],)
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to split at commas.')
 
     def test_Splits_At_Newline(self):
         # Test that correctly splits data into separate rows at newline character.
         test_data = StringIO('this,is,a,csv,file\nwith,two,lines,after,each-other!\n')
         expected_data = (['this', 'is', 'a', 'csv', 'file'], ['with', 'two', 'lines', 'after', 'each-other!'])
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to split at newlines')
 
     def test_Skips_Escaped_Characters(self):
         # Test that correctly skips escaped commas and newlines when parsing.
         test_data = StringIO('this,is,a,csv,file\nwith,"escaped\n",newlines,and,"com,mas"\n')
         expected_data = (['this', 'is', 'a', 'csv', 'file'], ['with', 'escaped\n', 'newlines', 'and', 'com,mas'])
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to escape the newlines/commas.')
 
     def test_No_Trailing_Newline(self):
         # Test that correctly returns all the data from files with no trailing newline, including the last line.
         test_data = StringIO('this,does,not,have,a,newline')
         expected_data = (['this', 'does', 'not', 'have', 'a', 'newline'],)
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to handle file with no trailing newline.')
 
     def test_Empty_File(self):
         # Test that it correctly handles empty files by returning and empty list.
         test_data = StringIO('')
         expected_data = ([],)
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to handle an empty file.')
 
     def test_String_Of_Commas(self):
         # Test that correctly handles a string of commas by returning a list of empty values.
         test_data = StringIO(',,,\n,,,')
         expected_data = (['', '', '', ''], ['', '', '', ''])
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to handle repeat commas.')
 
     def test_Escaping_Escape_Character(self):
         # Test that correctly handles escaping an escape character that is intended to be printed.
         test_data = StringIO(',""," "" "\n')
         expected_data = (['', '"', ' " '],)
-        for given, expected in zip(CSVParser.csv_reader(test_data.read()), expected_data):
+        for given, expected in zip(CSVParser.CSVReader(test_data.read()), expected_data):
             self.assertEqual(expected, given, f'{self.name}: Fails to escape the escape character.')
 
     def test_Large_Files(self):
         # Test that it correctly splits a large (contains no escapes).
         with open('Countries.csv', 'r') as f:
-            given, expected = [line for line in CSVParser.csv_reader(f.read())], []
+            given, expected = [line for line in CSVParser.CSVReader(f.read())], []
             f.seek(0)
             for line in f.readlines():
                 expected.append([item.strip('\"') for item in line.strip().split(',')])
@@ -74,11 +74,12 @@ class GetHeaderIndexes(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.name = cls.__name__
+        cls.headers = ('country', 'population', 'net change', 'land area', 'regions')
 
     def test_Headers_In_Order(self):
         # Test that get_header_indexes does indeed return indices.
         test_data, expected_out = ['country', 'population', 'net change', 'land area', 'regions'], (0, 1, 2, 3, 4)
-        given = CSVParser.get_header_indexes(test_data)
+        given = CSVParser.get_header_indexes(test_data, self.headers)
         self.assertEqual(expected_out, given, f'{self.name}: cannot return headers when in order')
 
     def test_Headers_In_Random_Order(self):
@@ -88,14 +89,14 @@ class GetHeaderIndexes(TestCase):
             new = test_data.copy()
             shuffle(new)
             expected_out = tuple(new.index(item) for item in test_data)
-            given = CSVParser.get_header_indexes(new)
+            given = CSVParser.get_header_indexes(new, self.headers)
             self.assertEqual(expected_out, given, f'{self.name}: cannot return correct index when order is randomised.')
 
     def test_Headers_With_Noise(self):
         # Test when there are more than just the default headers.
         test_data = ['country', 'Nonsense', 'population', 'more nonsense', 'net change', 'land area', 'lol', 'regions']
         expected_out = (0, 2, 4, 5, 7)
-        given = CSVParser.get_header_indexes(test_data)
+        given = CSVParser.get_header_indexes(test_data, self.headers)
         self.assertEqual(expected_out, given, f'{self.name}: cannot return correct index when headers include noise.')
 
     def test_Headers_In_Random_Order_With_Noise(self):
@@ -106,7 +107,7 @@ class GetHeaderIndexes(TestCase):
             new = test_data.copy()
             shuffle(new)
             expected_out = tuple(new.index(item) for item in correct_data)
-            given = CSVParser.get_header_indexes(new)
+            given = CSVParser.get_header_indexes(new, self.headers)
             self.assertEqual(expected_out, given, f'{self.name}: cannot return correct index with random noise.')
 
     def test_No_Headers(self):
@@ -114,10 +115,104 @@ class GetHeaderIndexes(TestCase):
         test_data = []
         flag = False
         try:
-            CSVParser.get_header_indexes(test_data)
+            CSVParser.get_header_indexes(test_data, self.headers)
         except ValueError:
             flag = True
         self.assertTrue(flag, f'{self.name}: does not raise exception if no headers are present.')
+
+
+class CSVCountryHandlerTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.name = cls.__name__
+
+    def setUp(self) -> None:
+        self.handler = CSVParser.CSVCountryHandler(
+            expected_headers=('country', 'population', 'net change', 'land area', 'regions')
+        )
+
+    def test_on_header_Headers_In_Order(self):
+        # Test that get_header_indexes does indeed return indices.
+        failure_msg = f'{self.name}: cannot return headers when in order'
+        self.handler.on_header(['country', 'population', 'net change', 'land area', 'regions'])
+        self.assertEqual((0, 1, 2, 3, 4), self.handler._header_indexes, failure_msg)
+
+    def test_on_header_Headers_In_Random_Order(self):
+        # Test when headers appear in a random order.
+        failure_msg = f'{self.name}: cannot return correct index when order is randomised.'
+        self.handler.on_header(['population', 'country', 'regions', 'land area', 'net change'])
+        self.assertEqual((1, 0, 4, 3, 2), self.handler._header_indexes, failure_msg)
+
+    def test_on_header_Headers_With_Noise(self):
+        # Test when there are more than just the default headers.
+        failure_msg = f'{self.name}: cannot return correct index when headers include noise.'
+        self.handler.on_header(
+            ['land area', 'lol', 'country', 'statistical bs', 'net change', 'population', 'hello there', 'regions']
+        )
+        self.assertEqual((2, 5, 4, 0, 7), self.handler._header_indexes, failure_msg)
+
+    def test_on_header_No_Headers(self):
+        # Tests that an error is raised if headers are not given.
+        failure_msg = f'{self.name}: fails to raise error when headers are missing.'
+        with self.assertRaises(ValueError) as exception:
+            self.handler.on_header([])
+        self.assertEqual(str(exception.exception), 'At least one header missing.', failure_msg)
+
+    def test_on_row_Row_Contains_None(self):
+        # Tests that an exception is raised if the row contains empty positions.
+        failure_msg = f'{self.name}: fails to raise error when row data is missing.'
+        self.handler._header_indexes = (0, 1, 2, 3, 4)
+        with self.assertRaises(ValueError) as exception:
+            self.handler.on_row(['hello', 'this', 'is', '', 'a', 'row'])
+        self.assertEqual(str(exception.exception), 'Row contains missing data', failure_msg)
+
+    def test_on_row_Type_Doesnt_Match(self):
+        # Tests that an exception is raised if the incoming data type does not match the header type.
+        failure_msg = f'{self.name}: fails to raise error when incoming data does not match header type.'
+        self.handler._header_indexes = (0, 1, 2, 3, 4)
+        with self.assertRaises(ValueError) as exception:
+            self.handler.on_row(['hello', '100', 'is', 'a', 'row'])
+        self.assertEqual(str(exception.exception), 'Type Mismatch', failure_msg)
+
+    def test_on_row_Before_On_Header(self):
+        # Tests that an exception is raised if on_row is called before _header_indexes is assigned.
+        failure_msg = f'{self.name}: fails to raise error when called before _header_indexes is assigned.'
+        with self.assertRaises(AttributeError) as exception:
+            self.handler.on_row(['anything'])
+        self.assertEqual(str(exception.exception), 'No Headers', failure_msg)
+
+
+class CountryTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.name = cls.__name__
+        cls.headers = (0, 1, 2, 3, 4)
+
+    def setUp(self) -> None:
+        self.country = CSVParser.Country
+
+    def test_Sort_By_Population(self):
+        # Test that key=country.rank sorts countries by population correctly.
+        failure_msg = f'{self.name} fails to sort by population (descending).'
+        data = (['c', 3, 0, 1, 'a'], ['b', 2, 0, 1, 'a'], ['a', 1, 0, 1, 'a'])
+        countries = sorted([self.country(data, self.headers) for data in data], key=self.country.rank)
+        self.assertEqual(['c', 'b', 'a'], [country.NAME for country in countries], failure_msg)
+
+    def test_Sort_By_Density(self):
+        # Test that key=country.rank sorts countries by density correctly.
+        failure_msg = f'{self.name} fails to sort by density (descending).'
+        data = (['c', 3, 0, 2, 'a'], ['b', 3, 0, 3, 'a'], ['a', 3, 0, 1, 'a'])
+        countries = sorted([self.country(data, self.headers) for data in data], key=self.country.rank)
+        self.assertEqual(['a', 'c', 'b'], [country.NAME for country in countries], failure_msg)
+
+    def test_Sort_By_Name(self):
+        # Test that key=country.rank sorts countries by name correctly.
+        failure_msg = f'{self.name} fails to sort by name (ascending).'
+        data = (['c', 1, 0, 1, 'a'], ['b', 1, 0, 1, 'a'], ['a', 1, 0, 1, 'a'])
+        countries = sorted([self.country(data, self.headers) for data in data], key=self.country.rank)
+        self.assertEqual(['a', 'b', 'c'], [country.NAME for country in countries], failure_msg)
 
 
 class MainFunctionTest(TestCase):
